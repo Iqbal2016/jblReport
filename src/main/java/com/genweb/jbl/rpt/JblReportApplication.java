ea0a52b3b780c1756def2aa7b6d9f03f4da6e39d
Iqbal Hossain <hossain.iqbal@genweb.com>
2021-12-01 18:53:58 +0600

    add

--- src/main/java/com/genweb/jbl/rpt/JblReportApplication.java
+++ src/main/java/com/genweb/jbl/rpt/JblReportApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JblReportApplication {
+public class JblReportApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JblReportApplication.class, args);
